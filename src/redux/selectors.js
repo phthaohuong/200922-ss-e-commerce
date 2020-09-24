2c40c6791873607db84649746f4b2ed30b28ce2d
phthaohuong <thaohuongtqt1603@gmail.com>
2020-09-24 13:48:09 +0700

    Import_React_in_selector.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,10 +1,9 @@
+import React from "react";
 import * as R from "ramda";
 
 export const getPhoneById = (state, id) => R.prop(id, state.phones);
 
 export const getPhones = (state) => {
-  <div>
-    const phones = R.map((id) => getPhoneById(state, id), state.phonesPage.ids);
-    return phones;
-  </div>;
+  const phones = R.map((id) => getPhoneById(state, id), state.phonesPage.ids);
+  return phones;
 };
